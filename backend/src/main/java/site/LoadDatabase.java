0ef308f132cbb35f99635ad9bb803037a613dd3e
berkam <berkam15@mail.ru>
2021-09-13 22:02:17 +0300

    Commit initPersonInDatabase

--- backend/src/main/java/site/LoadDatabase.java
+++ backend/src/main/java/site/LoadDatabase.java
@@ -4,10 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import site.model.*;
+import site.model.ProductRepository;
 import site.utils.ProductGenerator;
-
-import java.math.BigDecimal;
 
 @Configuration
 @Slf4j
@@ -21,12 +19,12 @@ public class LoadDatabase {
         };
     }
 
-    @Bean
-    CommandLineRunner initPersonInDatabase(UserRepository repository) {
-        return args -> {
-            Address address = new Address("Сектор", "Солнце", "Земля");
-            log.info("Preloading " + repository.save(new Person("berkam", "1111",
-                    new BigDecimal("1000"), new Basket(), address)));
-        };
-    }
+//    @Bean
+//    CommandLineRunner initPersonInDatabase(UserRepository repository) {
+//        return args -> {
+//            Address address = new Address("Сектор", "Солнце", "Земля");
+//            log.info("Preloading " + repository.save(new Person("berkam", "1111",
+//                    new BigDecimal("1000"), new Basket(), address)));
+//        };
+//    }
 }

5e6adca7a9dfc26fa9ac4bf53d6dd466f94e862f
berkam <berkam15@mail.ru>
2021-09-25 22:42:33 +0300

    Clear tests code

--- integration_tests/src/test/java/site/login/RegistrationTest.java
+++ integration_tests/src/test/java/site/login/RegistrationTest.java
@@ -2,30 +2,18 @@ package site.login;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import site.steps.RegistrationSteps;
 import site.utils.TestData;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 @SpringBootTest
-@AutoConfigureMockMvc
 public class RegistrationTest {
 
     @Autowired
-    private MockMvc mockMvc;
+    RegistrationSteps registrationSteps;
 
     @Test
-    public void createUser() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders
-                        .post("/createUser")
-                        .param("email", TestData.getRandomEmail())
-                        .param("password", TestData.getRandomPassword())
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().is2xxSuccessful());
+    public void createUser() {
+        registrationSteps.createUser(TestData.getRandomEmail(), TestData.getRandomEmail());
     }
 }
